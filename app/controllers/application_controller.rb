5fd42b7483510f0aaddee39b269e7ad37d5b4e27
EdenilsonGitHub <edenilson@etica-ti.com.br>
2020-05-23 01:29:20 -0300

    PAD-35 - Corrigido login e criptografado senha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 # encoding: UTF-8
 class ApplicationController < ActionController::Base
-    before_action :usuario_logado, :logado, except: [:login, :logar]
+    before_action :usuario_logado, except: [:login, :logar]
 
     def usuario_logado
         @usuario_logado = Usuario.find_by_id(cookies[:id])
         @usuarios = Usuario.all
-    end
-
-    def logado
-        if @usuario_logado == nil
-            redirect_to root_path
-        end
     end
 
     def sair
