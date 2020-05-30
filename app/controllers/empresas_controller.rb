514a33f8635e443b8e8d799b59d35f4fe46505d1
luciux <l_u_cio@hotmail.com>
2020-05-30 06:33:12 -0700

    PAD-19 - Realizado a edição do registro de empresas

--- app/controllers/empresas_controller.rb
+++ app/controllers/empresas_controller.rb
@@ -8,15 +8,28 @@ class EmpresasController < ApplicationController
     def new
         @empresa = Empresa.new
     end
-
+    
     def create
-        @empresa = Empresa.new(params.require(:empresa).permit(:nome, :cnpj, :telefone, :email, :ativo, :sigla, :razao_social, :logo))
+        @empresa = Empresa.new(params.require(:empresa).permit(:nome, :cnpj, :telefone, :email, :ativo, :sigla, :razao_social, :logo, :pais, :estado,
+                    :cidade, :rua, :numero, :complemento, :cep))
         if @empresa.save
             redirect_to empresas_path            
         else
             render action: 'new'
         end
     end
+    
+    def edit
+    end
+
+    def update
+        if @empresa.update_attributes(params.require(:empresa).permit(:nome, :cnpj, :telefone, :email, :ativo, :sigla, :razao_social, :logo, :pais, :estado,
+            :cidade, :rua, :numero, :complemento, :cep))
+            redirect_to empresas_path
+        else
+            render 'edit'
+        end
+    end    
 
     private
 
