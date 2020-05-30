2cf1aa922d0f2a0c358d07306fe2981c4d931ae9
luciux <l_u_cio@hotmail.com>
2020-05-30 07:11:36 -0700

    PAD-55 - Adicionado validações para o cadastro de empresas

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -3,6 +3,12 @@ class Empresa < ActiveRecord::Base
 
     include Paperclip::Glue
 
+    validates :nome, :cnpj, :email, :razao_social, :sigla, presence: true
+    validates :email, :cnpj, :razao_social, :sigla, :nome, uniqueness: true    
+    validates :cnpj, length: {is: 14}, allow_blank: false
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :cep, length: {is: 8}, allow_blank: false
+
     has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }
     validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 
