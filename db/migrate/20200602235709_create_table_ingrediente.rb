8bae4f442d7d367dd7d7e75af3a980343e3926c6
luciux <l_u_cio@hotmail.com>
2020-06-02 17:08:03 -0700

    PAD-64 - Criação da migração da tabela ingredientes

---
+++ db/migrate/20200602235709_create_table_ingrediente.rb
@@ -0,0 +1,10 @@
+class CreateTableIngrediente < ActiveRecord::Migration[6.0]
+  def change
+    create_table   :table_ingredientes do |t|
+      t.string     :nome
+      t.boolean    :alergicos
+      t.string     :observacao     
+      t.timestamps   
+    end
+  end
+end
