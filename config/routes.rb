ed69f3b3f529e135c607d5e1a9336f579772d617
Edenilson Luiz de Almeida <edenilson@etica-ti.com.br>
2020-06-08 13:58:20 -0300

    PAD-62 - Iniciado dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :empresas
+  resources :ingredientes
   resources :produtos
   resources :usuarios
-  resources :ingredientes
 
-  
+  resources :dashboards, only: :index
   resources :home, only: :index
 
   post 'logar', to: 'usuarios#logar'   , as: :logar
