f79cf2d338f981835151311683a58633e6e07162
luciux <l_u_cio@hotmail.com>
2020-06-08 18:28:05 -0700

    PAD-68 - Cadastro para os parceiros

--- app/models/parceiro.rb
+++ app/models/parceiro.rb
@@ -3,7 +3,7 @@ class Parceiro < ActiveRecord::Base
     
     validates :nome, :cnpj, :email, :servico_prestado, presence: true
     validates :nome, :cnpj, :email, uniqueness: true
-    validates :servico_prestado, length: { minimum: 10, message: 'é muito curto (mínimo: 10 caracteres)' }
+    validates :servico_prestado, length: { minimum: 5, message: 'é muito curto (mínimo: 5 caracteres)' }
     validates :nome, length: { maximum: 50, message: 'é muito grande (máximo: 50 caracteres)' }
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :cnpj, length: {is: 14}, allow_blank: false
