fc778139407e6ed59a70c04423d6c590bf2eed91
Edenilson <56543408+EdenilsonGitHub@users.noreply.github.com>
2020-06-10 19:20:45 -0300

    Update ingrediente.rb

--- app/models/ingrediente.rb
+++ app/models/ingrediente.rb
@@ -1,8 +1,8 @@
 class Ingrediente < ActiveRecord::Base
     self.table_name = 'table_ingredientes'
 
-    has_many :table_rel_prod_ing
-    
+    has_many :table_rel_prod_ing, class_name: 'Relproding', foreign_key: :ingrediente_id
+
     validates :nome, :alergicos, :observacao, presence: true
     validates :nome, uniqueness: true
     validates :observacao, length: { minimum: 10, message: 'é muito curto (mínimo: 10 caracteres)' }
