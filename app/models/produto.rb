adf49a58bdbed48398d007cca9ab42fe7e93965e
EdenilsonGitHub <edenilson@etica-ti.com.br>
2020-06-17 22:17:00 -0300

    PAD-99 - Validações de novos campos do produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -14,4 +14,15 @@ class Produto < ActiveRecord::Base
     validates :codigo_de_barras, length: { minimum: 12, message: 'é muito curto (mínimo: 12 caracteres)' }
     validates :codigo_de_barras, length: { maximum: 13, message: 'é muito longo (máximo: 13 caracteres)' }
     validates :nome, length: { maximum: 55, message: 'é muito longo (máximo: 55 caracteres)' }
+    validates :estrela, numericality: { only_integer: true }
+    validates :estrela, length: {is: 1, message: 'não possui o tamanho esperado (1 caractere)'}, allow_blank: false
+    validates :valor_promocao, :preco, :estrela, numericality: true
+
+    def estrela_entre_1_5?(produto)
+        if produto.estrela >= 0 && produto.estrela <= 5
+            return true
+        else
+            return false
+        end
+    end
 end 
