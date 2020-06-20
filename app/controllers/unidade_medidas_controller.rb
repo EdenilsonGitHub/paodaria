6ddfbfa6e1670fc434bbcc544d103ad2c64dbc8a
EdenilsonGitHub <edenilson@etica-ti.com.br>
2020-06-20 17:12:45 -0300

    PAD-115 - Criado edit/update e destroy das unidades de medidas

--- app/controllers/unidade_medidas_controller.rb
+++ app/controllers/unidade_medidas_controller.rb
@@ -1,5 +1,11 @@
 class UnidadeMedidasController < ApplicationController
 
+    before_action :procurar_id, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @unidade_medidas = UnidadeMedida.all
+    end
+
     def new
         @unidade_medida = UnidadeMedida.new
     end
@@ -12,5 +18,27 @@ class UnidadeMedidasController < ApplicationController
             render action: 'new'
         end
     end
+
+    def edit
+    end
+
+    def update 
+        if @unidade_medida.update_attributes(params.require(:unidade_medida).permit(:nome))
+            redirect_to unidade_medidas_path
+        else
+            render 'edit'
+        end            
+    end
+
+    def destroy
+        @unidade_medida.destroy
+        redirect_to unidade_medidas_path
+    end
+
+    private
+
+    def procurar_id
+        @unidade_medida = UnidadeMedida.find_by_id(params[:id])
+    end    
 
 end
