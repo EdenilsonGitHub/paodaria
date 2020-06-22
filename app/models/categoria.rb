0121d979c7f247ebd552e23a55dd39320f5c334f
EdenilsonGitHub <edenilson@etica-ti.com.br>
2020-06-22 19:33:04 -0300

    Fix

--- app/models/categoria.rb
+++
@@ -1,4 +0,0 @@
-class Categoria < ActiveRecord::Base
-    self.table_name = 'categ'
-
-end
--- db/migrate/20200622154919_add_column_categ_to_prod.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnCategToProd < ActiveRecord::Migration[6.0]
-  def up
-    add_column :prod, :categoria_id, :integer
-  end
-
-  def down
-    remove_column :prod, :categoria_id
-  end
-end
